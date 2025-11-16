9fa865298fbb571cda821fee1abf184349ba7d34
Allwell <allwellazubike@gmail.com>
2025-11-16 12:11:06 +0100

    still trying to deploy

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs", // Scans all EJS files in the views folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
